ddfb1b826171d261e6d34abb500041c8600807c0
Washington Amedu <washingtonamedu@gmail.com>
2026-05-31 23:36:35 +0100

    feat: add complete modular backend and frontend project structure

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -1,5 +1,5 @@
-const { SUPER_ADMIN_EMAIL } = require("../config/constants");const mongoose = require("mongoose");
-const { SUPER_ADMIN_EMAIL } = require("../../config/constants");
+const mongoose = require("mongoose");
+const { SUPER_ADMIN_EMAIL } = require("../config/constants");
 
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true, lowercase: true, trim: true },
